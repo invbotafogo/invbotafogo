f8b8a02a5a30b0c9e79cd7a0ad997ee405830806
matheusnsp <116042913+matheusnsp@users.noreply.github.com>
2025-03-03 23:59:55 -0300

    Novo visual na pagina

---
+++ invb_site/assets/js/script.js
@@ -0,0 +1,14 @@
+// Efeito de fade-out para a seção "Bem-vindo"
+window.addEventListener('scroll', function() {
+    const heroSection = document.querySelector('.section-hero');
+    const cultosSection = document.querySelector('.section-cultos');
+    
+    if (window.scrollY > 100) { // Quando o scroll for maior que 100px
+        heroSection.classList.add('fade-out');
+        cultosSection.style.opacity = 1; // Torna a seção de cultos visível
+        cultosSection.style.transition = 'opacity 1s ease-in-out'; // Aparece suavemente
+    } else {
+        heroSection.classList.remove('fade-out');
+        cultosSection.style.opacity = 0; // Deixa a seção de cultos invisível novamente
+    }
+});
