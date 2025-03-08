9b18787c6a40cd40f13ea7e52b888af0787e9eda
Jean Felix <jean.s.felix@gmail.com>
2025-03-08 02:04:35 -0300

    Corrige hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const glob = require('glob'); // Para encontrar arquivos HTML automaticamente
 
 module.exports = {
+  cache: false,
   entry: './src/js/script.js',  // Arquivo de entrada principal
   output: {
     filename: 'bundle.js',  // Arquivo JS empacotado
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/invbotafogo/'
+    clean: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/invbotafogo/' : '/',
   },
   module: {
     rules: [
@@ -33,12 +35,17 @@ module.exports = {
     ...glob.sync('./src/**/*.html').map(file => {
       return new HtmlWebpackPlugin({
         template: file,
-        filename: path.basename(file), // Usa o nome do arquivo para o destino
+        filename: path.relative("./src", file), // Usa o nome do arquivo para o destino
       });
     }),
   ],
+  cache: false,
   devServer: {
-    static: './dist',
-    open: true,
-  },
+    static: path.resolve(__dirname, 'dist'), // Diretório dos arquivos estáticos
+    port: 8080,
+    open: true, // Abre o navegador automaticamente
+    hot: true, // Ativa o HMR (Hot Module Replacement)
+    liveReload: true, // Atualiza a página quando há mudanças
+    watchFiles: ['src/**/*'], // Observa mudanças em arquivos HTML, CSS e JS
+  }
 };
