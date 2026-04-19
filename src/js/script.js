5216d4f6b3458fcbabc8ce502f1ffd3976f57917
allanbfr77 <alan.tcn1@gmail.com>
2026-04-19 16:22:36 -0300

    add pdf cristologia parte II

--- src/js/script.js
+++ src/js/script.js
@@ -207,8 +207,8 @@ async function carregarEstudosBiblicos(tema) {
     if (tema == "cristologia") {
         const aulas = [
             { titulo: "Cristologia - Parte I", imagem: `${basePathImgCR}/1.png`, pdf: `${basePath}/CRISTOLOGIA_Aula1.pdf` },
-            { titulo: "Cristologia - Parte II", imagem: `${basePathImgCR}/1.png`, pdf: `${basePath}/CRISTOLOGIA_Aula2.pdf` },
-            //teste
+            { titulo: "Cristologia - Parte II", imagem: `${basePathImgCR}/2.png`, pdf: `${basePath}/CRISTOLOGIA_Aula2.pdf` },
+            //{ titulo: "Cristologia - Parte III", imagem: `${basePathImgCR}/3.png`, pdf: `${basePath}/CRISTOLOGIA_Aula3.pdf` },
         ];
 
         aulas.forEach(aula => {
